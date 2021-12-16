34b7af9e70722c5f9573ca9d4491885b9f768e25
RodrigoABFerreiro <95659871+RodrigoABFerreiro@users.noreply.github.com>
2021-12-16 17:26:12 -0300

    Atualizações 16-12-21

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,34 @@
 $('.carousel').carousel({
     interval: 5
 })
+
+function validar_dados() {
+    var nome = form01.form_nome.value
+    var email = form01.form_email.value
+    var assunto = form01.form_assunto.value
+    var mensagem = form01.form_mensagem.value
+
+    if (form_nome == "") {
+      alert("Preencha o campo Nome corretamente.")
+      form01.nome.focus()
+      return false
+    }
+
+    if (email == "") {
+      alert("Preencha o campo E-Mail corretamente.")
+      form01.email.focus()
+      return false
+    }
+
+    if (assunto == "") {
+      alert("Preencha o campo Assunto corretamente.")
+      form01.senha.focus()
+      return false
+    }
+
+    if (form_mensagem == "") {
+      alert("Preencha o campo Mensagem corretamente.")
+      form01.smensagem.focus()
+      return false
+    }
+  }
